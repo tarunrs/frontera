7d88c2e8a437dc45dc3bda12cef3396590c4b346
Tarun R S <tarunrs@gmail.com>
2016-12-20 07:40:41 +0000

    Using CrawlSpider and rules to fetch next links to crawl

--- examples/cluster/bc/spiders/bc.py
+++ examples/cluster/bc/spiders/bc.py
@@ -4,22 +4,21 @@ from scrapy.http import Request
 from scrapy.http.response.html import HtmlResponse
 from scrapy.linkextractors import LinkExtractor
 from scrapy import signals
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-class BCSpider(Spider):
+
+class BCSpider(CrawlSpider):
     name = 'bc'
+    a = open("seeds.txt").readlines()
+    a = [b.replace("http://", "") for b in a]
+    a = [b.replace("www.", "") for b in a]
+    a = [b.strip("\n\/") for b in a]
+    allowed_domains = a
 
-    def __init__(self, *args, **kwargs):
-        super(BCSpider, self).__init__(*args, **kwargs)
-        self.le = LinkExtractor()
-
-    def parse(self, response):
-        if not isinstance(response, HtmlResponse):
-            return
-
-        for link in self.le.extract_links(response):
-            r = Request(url=link.url)
-            r.meta.update(link_text=link.text)
-            yield r
+    rules = (
+        Rule(LinkExtractor(allow_domains=allowed_domains), follow=True),
+    )
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
