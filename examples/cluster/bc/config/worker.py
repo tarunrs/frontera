5c66368cf8e8ed3bac99dc376e67305d20695910
Tarun R S <tarun.s@minoty.com>
2017-01-06 17:48:03 +0530

    No longer dropping HBase tables

--- examples/cluster/bc/config/worker.py
+++ examples/cluster/bc/config/worker.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .common import *
 
 BACKEND = 'frontera.contrib.backends.hbase.HBaseBackend'
-HBASE_DROP_ALL_TABLES = True
+HBASE_DROP_ALL_TABLES = False
 HBASE_TIMEOUT = 30000
 HBASE_THRIFT_HOST = "10.2.0.17"
 MAX_NEXT_REQUESTS = 2048
